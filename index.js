25bb8b3bf1b9f34d150e38f92b30a97096650413
John Carmichael <jc@stackeducation.com>
2019-09-04 15:43:35 -0400

    Added POST route for creating teams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const teams = require('./teams.json')
 
 const app = express()
@@ -17,6 +18,20 @@ app.get('/teams/:identifier', (request, response) => {
   } else {
     response.sendStatus(404)
   }
+})
+
+app.post('/teams', bodyParser.json(), (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division,
+  } = request.body
+
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    response.status(400).send('The following fields are required: id, location, mascot, abbreviation, conference, division')
+  }
+
+  teams.push({ id, location, mascot, abbreviation, conference, division })
+
+  response.status(201).send({ id, location, mascot, abbreviation, conference, division })
 })
 
 app.listen(1337, () => { console.log('Listening on port 1337') })
