e70aa2d4d8e19328d0d4167e002f95d0713f9b56
Sasha Gemis <sashgems@sashas-air.worcester.local>
2019-11-20 17:36:17 -0500

    changes

--- index.js
+++ index.js
@@ -1,42 +1,79 @@
 const express = require('express')
-var app = express()
-var teams = require('./teams.json')
 
+// Creating an instance of an Express application
+const app = express()
+const teams = require('./teams.json')
 
-app.get('/teams',(request,response) => {
+// Middleware that takes a string and turns it into a JS object out of that string that we can use
+const bodyParser = require('body-parser')
+
+app.get('/teams', (request, response) => {
     response.send(teams)
 })
-    
-app.get('/teams/:id/', (request, response) => {
-    let teamById = teams.filter( (item) => {
-        return item.id == request.params.id
-        
-    })
-    response.send(teamById)
 
+app.get('/teams/:filter', (request, response) => {
+    let filter = request.params.filter
+    let result = teams.filter((team) => {
+        
+        // Or else String(team.id) === filter
+        return team.id == filter || 
+               team.abbreviation === filter ||
+               team.division === filter                  
+    })
+
+    // Just sends back locations when inputting any of the fields above
+    /*let locations = result.map((team) => {
+        return team.location
+    })*/
+    response.send(result) // response.send(locations) to just send back location and uncomment lines 25-27
+    console.log(filter)
 })
 
-app.get('/teams/:id/:abbreviation/', (request, response) => {
-    let teamByAbbreviation = teams.filter( (team) => {
-       return team.abbreviation == request.params.abbreviation
-    })  
-    
-    response.send(teamByAbbreviation)
-    
-}) 
+app.post('/teams', bodyParser.json(), (request, response) => {
+    const body = request.body
+
+    if (!body.id) {
+        body.id = teams.length + 1
+    }
+
+    if (
+       !body.location || 
+       !body.mascot || 
+       !body.abbreviation || 
+       !body.conference || 
+       !body.division
+    ) {
+      response.status(400).send('The following attributes are required: location, mascot, abbreviation, conference and division.')
+    }
+
+    const newTeam = teams.concat(body)
+
+    console.log({body})
+    response.send(newTeam)
+})
+
+app.delete('/teams/:id', (request, response) => {
+    // Look up the team
+    // If it doesn't exist, return 404
+    const team = teams.find(t => t.id === parseInt(request.params.id));
+
+    if (!team) {
+        response.status(404).send('The team with the given ID does not exist.')
+
+    } else {
+    // Otherwise, delete it
+        const index = teams.indexOf(team);
+        teams.splice(index, 1);
+    }
+
+    // Return successful delete attempt message
+    response.send('Successfully deleted the team with ID: ' + team.id)
+})
 
 app.all('*', (request, response) => {
-    console.log({request})
-    response.send('you are a unicorn and are on the right track, keep going!')
+    response.send('404 - Not Found. Please be more specific.')
 })
 
-app.listen(1338, () => {
-    console.log('We are listening')
+app.listen(1337, () => {
+    console.log('Server is up and running.')
 })
-
-// const port = process.env.PORT || 3000; 
-// app.listen(port, () => {
-//     console.log(`Listtening on ${port}...`)
-// })
-
-module.exports = app
